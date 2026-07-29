9446209e72e5628aa6dfdcfdd696a96461c3ede6
MuhammadKhaidir <khaidirmuhamad0@gmail.com>
2026-07-29 11:47:35 +0700

    fix: samakan casing require musicStore jadi lowercase

--- routes/music.js
+++ routes/music.js
@@ -38,7 +38,7 @@ const path = require('path');
 const crypto = require('crypto');
 const fs = require('fs');
 
-const store = require('../data/MusicStore');
+const store = require('../data/musicStore');
 
 const router = express.Router();
 
